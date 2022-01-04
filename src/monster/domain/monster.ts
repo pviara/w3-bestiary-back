c21b2c5c869cad8e0191f707c4ef79c537945853
Pierre Viara <pierre.viara@aktome.fr>
2022-01-04 17:58:39 +0100

    feat(monster): add monster classes

---
+++ src/monster/domain/monster.ts
@@ -0,0 +1,39 @@
+export class Monster {
+    constructor(
+        readonly _id: string,
+        readonly code: string,
+        readonly textes: MonsterTextes,
+        readonly weakspots: MonsterWeakspots
+    ) {}
+}
+
+export class MonsterTextes {
+    constructor(
+        readonly description: string,
+        readonly quote: MonsterTextesQuote,
+    ) {}
+}
+
+export class MonsterTextesQuote {
+    constructor(
+        readonly author: MonsterTextesQuoteAuthor,
+        readonly text: string
+    ) {}
+}
+
+export class MonsterTextesQuoteAuthor {
+    constructor(
+        readonly firstname: string,
+        readonly lastname: string,
+        readonly title: string,
+    ) {}
+}
+
+export class MonsterWeakspots {
+    constructor(
+        readonly bombs: string[],
+        readonly oils: string[],
+        readonly potions: string[],
+        readonly signs: string[]
+    ) {}
+}
