332a342b1fff91b0ef1c2b5a5919c20ab709861f
Pierre Viara <pierre.viara@aktome.fr>
2022-01-04 19:19:04 +0100

    feat(monster): replace injected repo in controller by CQRS handler

--- src/monster/monster.module.ts
+++ src/monster/monster.module.ts
@@ -3,16 +3,24 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MonsterController } from './presentation/monster.controller';
 import { MonsterRepoProvider } from './persistence/monster-repository.provider';
 import { monsterSchema } from './persistence/monster-entity';
+import { CqrsModule } from '@nestjs/cqrs';
+import { GetAllMonstersHandler } from './application/queries/get-all-monsters.query';
+
+const queryHandlers = [GetAllMonstersHandler];
 
 @Module({
     controllers: [MonsterController],
     exports: [],
     imports: [
+        CqrsModule,
         MongooseModule.forFeature([{
             name: 'Monster',
             schema: monsterSchema
         }])
     ],
-    providers: [MonsterRepoProvider]
+    providers: [
+        MonsterRepoProvider,
+        ...queryHandlers
+    ]
 })
 export class MonsterModule {}
--- src/monster/presentation/monster.controller.ts
+++ src/monster/presentation/monster.controller.ts
@@ -1,18 +1,23 @@
-import { BadRequestException, Controller, Get, Inject, Query } from '@nestjs/common';
-import { IMonsterRepository } from '../application/monster-repository.interface';
+import { BadRequestException, Controller, Get, NotFoundException, Query } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import { GetAllMonstersQuery } from '../application/queries/get-all-monsters.query';
 
 @Controller()
 export class MonsterController {
-    constructor(
-        @Inject('MonsterRepo')
-        private readonly _monsterRepository: IMonsterRepository
-    ) {}
+    constructor(private readonly _queryBus: QueryBus) {}
     
     @Get()
     async getAll(@Query('lang') lang: string) {
         if (!lang) {
             throw new BadRequestException('A language must be provided in query params.');
         }
-        return await this._monsterRepository.getAll(lang);
+
+        const query = new GetAllMonstersQuery(lang);
+        const result = await this._queryBus.execute(query);
+        if (!result) {
+            throw new NotFoundException('No monster was found');
+        }
+        
+        return result;
     }
 }
