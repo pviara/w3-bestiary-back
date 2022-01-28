3c96a3bc9e61ece32b1f1a2cbe8571a26f528e2f
Pierre Viara <pierre.viara@outlook.com>
2022-01-28 14:57:53 +0100

    tech : apply linter

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import * as morgan from 'morgan';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    
+
     morgan.token('angle-bracket', () => '>');
     morgan.token('timestamp', () => new Date().toISOString());
     app.use(
-        morgan(':angle-bracket :timestamp :method :url :status :res[content-length] - :response-time ms')
+        morgan(
+            ':angle-bracket :timestamp :method :url :status :res[content-length] - :response-time ms',
+        ),
     );
 
     await app.listen(3000);
