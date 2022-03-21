16a7bbb08a27cb5cdc044b44eb86545eefefc972
Pierre Viara <pierre.viara@aktome.fr>
2022-03-21 13:28:42 +0100

    tech(items): add tests on item controller

---
+++ src/item/presentation/item.controller.spec.ts
@@ -0,0 +1,50 @@
+import { createMock } from 'ts-auto-mock';
+import { Error } from '../../application/error';
+import { GetAllItemsURLQuery } from './DTO/get-all-items.url-query';
+import { HttpException } from '@nestjs/common';
+import { Item } from '../domain/item';
+import { ItemController } from './item.controller';
+import { method, On } from 'ts-auto-mock/extension';
+import { QueryBus } from '@nestjs/cqrs';
+import { Result } from '../../application/result';
+import { when } from 'jest-when';
+
+describe('ItemController', () => {
+    let sut: ItemController;
+
+    let queryBusMock: QueryBus;
+
+    beforeEach(() => {
+        queryBusMock = createMock<QueryBus>();
+
+        sut = new ItemController(queryBusMock);
+    });
+
+    describe('getAll', () => {
+        it('should call execute on queryBus with given query', async () => {
+            const getAllItemsURLQuery: GetAllItemsURLQuery = {
+                lang: 'lang',
+            };
+
+            const queryBusMockExecute = On(queryBusMock).get(method('execute'));
+            when(queryBusMockExecute).mockReturnValue(new Result<Item[]>([]));
+
+            await sut.getAll(getAllItemsURLQuery);
+
+            expect(queryBusMock.execute).toBeCalled();
+        });
+
+        it('should throw an HttpException when execution result is an Error', async () => {
+            const getAllItemsURLQuery: GetAllItemsURLQuery = {
+                lang: 'lang',
+            };
+
+            const queryBusMockExecute = On(queryBusMock).get(method('execute'));
+            when(queryBusMockExecute).mockReturnValue(new Error(0, ''));
+
+            expect(
+                async () => await sut.getAll(getAllItemsURLQuery),
+            ).rejects.toThrow(HttpException);
+        });
+    });
+});
