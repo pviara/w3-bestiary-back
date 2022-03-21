69f5cae8a5d320eda967039efd7e8803b24afa28
Pierre Viara <pierre.viara@outlook.com>
2022-03-21 21:47:25 +0100

    feat(controllers): change the way HTTP errors are thrown

--- src/application/error.ts
+++ src/application/error.ts
@@ -1,12 +1,3 @@
-import { HttpException } from '@nestjs/common';
-
 export class Error {
-    constructor(
-        private readonly _code: number,
-        private readonly _message: string,
-    ) {}
-
-    throw(): void {
-        throw new HttpException(this._message, this._code);
-    }
+    constructor(readonly code: number, readonly message: string) {}
 }
