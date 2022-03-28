c7811de35c00c7a380c6bd42754529326f2f286d
Pierre Viara <pierre.viara@outlook.com>
2022-03-28 17:38:23 +0200

    feat(app): add logging system

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { createStream } from 'rotating-file-stream';
 import { NestFactory } from '@nestjs/core';
 import * as morgan from 'morgan';
 
@@ -6,11 +8,20 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
 
+    const configService = app.get<ConfigService>(ConfigService);
+    const LOGS_PATH = configService.get<string>('LOGS_PATH');
+
+    const logStream = createStream('logs.log', {
+        interval: '1d',
+        path: LOGS_PATH,
+    });
+
     morgan.token('angle-bracket', () => '>');
     morgan.token('timestamp', () => new Date().toISOString());
     app.use(
         morgan(
             ':angle-bracket :timestamp :method :url :status :res[content-length] - :response-time ms',
+            { stream: logStream },
         ),
     );
 
