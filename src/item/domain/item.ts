b42528ca76b8f992f38b8f616d1e231d2a83a84d
Pierre Viara <pierre.viara@outlook.com>
2022-04-03 22:18:06 +0200

    feat(item): add swagger doc to item module

--- src/item/domain/item.ts
+++ src/item/domain/item.ts
@@ -1,3 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Item {
-    constructor(readonly code: string, readonly name: string) {}
+    @ApiProperty()
+    readonly code: string;
+
+    @ApiProperty()
+    readonly name: string;
+
+    constructor(code: string, name: string) {
+        this.code = code;
+        this.name = name;
+    }
 }
