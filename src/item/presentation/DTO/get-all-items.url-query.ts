d59b5a8037ff13535bf90ac37287eab9c5d4035f
Pierre Viara <pierre.viara@outlook.com>
2022-04-03 22:18:06 +0200

    feat(item): add swagger doc to item module

--- src/item/presentation/DTO/get-all-items.url-query.ts
+++ src/item/presentation/DTO/get-all-items.url-query.ts
@@ -1,6 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDefined, IsString } from 'class-validator';
 
 export class GetAllItemsURLQuery {
+    @ApiProperty({
+        required: true,
+    })
     @IsDefined()
     @IsString()
     lang: string;
