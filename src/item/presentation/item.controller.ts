fc06e49f8a22b90c8821e79fdf92d2740a802475
Pierre Viara <pierre.viara@outlook.com>
2022-04-03 22:18:06 +0200

    feat(item): add swagger doc to item module

--- src/item/presentation/item.controller.ts
+++ src/item/presentation/item.controller.ts
@@ -1,4 +1,11 @@
 import {
+    ApiBadRequestResponse,
+    ApiNotFoundResponse,
+    ApiOkResponse,
+    ApiOperation,
+    ApiTags,
+} from '@nestjs/swagger';
+import {
     Controller,
     Get,
     HttpException,
@@ -12,10 +19,26 @@ import { Item } from '../domain/item';
 import { QueryBus } from '@nestjs/cqrs';
 import { Result } from '../../application/result';
 
+@ApiTags('item')
 @Controller()
 export class ItemController {
     constructor(private readonly _queryBus: QueryBus) {}
 
+    @ApiOperation({
+        description: 'Get items for the given language.',
+    })
+    @ApiBadRequestResponse({
+        description:
+            'No language was provided or no such language exists in database.',
+    })
+    @ApiOkResponse({
+        description: 'Retrieved items for the given language.',
+        type: Item,
+        isArray: true,
+    })
+    @ApiNotFoundResponse({
+        description: 'At least one item was not found for the given language.',
+    })
     @Get()
     async getAll(@Query(new ValidationPipe()) query: GetAllItemsURLQuery) {
         const getAllItemsQuery = new GetAllItemsQuery(query.lang);
