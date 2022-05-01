09cceef3820e9c7654d9e4e435b47a3b667db42a
Pierre Viara <pierre.viara@outlook.com>
2022-05-01 16:29:24 +0200

    feat(item): add GetItemThumbnail handler

--- src/item/application/queries/get-item-thumbnail.handler.ts
+++ src/item/application/queries/get-item-thumbnail.handler.ts
@@ -1,9 +1,38 @@
-import { FileFolder } from 'src/file/application/file-service.interface';
-import { IQuery } from '@nestjs/cqrs';
+import { Error } from '../../../application/error';
+import {
+    FileFolder,
+    FileFormat,
+    IFileService,
+} from '../../../file/application/file-service.interface';
+import { Inject } from '@nestjs/common';
+import { IQuery, IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { ReadStream } from 'fs';
+import { Result } from '../../../application/result';
 
 export class GetItemThumbnailQuery implements IQuery {
+    constructor(readonly code: string, readonly folder: FileFolder) {}
+}
+
+const FileService = () => Inject('FileService');
+
+@QueryHandler(GetItemThumbnailQuery)
+export class GetItemThumbnailHandler
+    implements IQueryHandler<GetItemThumbnailQuery>
+{
     constructor(
-        readonly code: string,
-        readonly folder: FileFolder,
+        @FileService()
+        private readonly _fileService: IFileService,
     ) {}
+
+    async execute(
+        query: GetItemThumbnailQuery,
+    ): Promise<Result<ReadStream> | Error> {
+        const filePath = this._fileService.computeFilePath(
+            query.folder,
+            FileFormat.PNG,
+            query.code,
+        );
+
+        return this._fileService.getFileStream(filePath);
+    }
 }
