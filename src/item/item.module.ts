d9ef03d98eb87022463c083d564e146ac29ec58d
Pierre Viara <pierre.viara@outlook.com>
2022-05-02 16:30:41 +0200

    fix(item): add missing query handler in ItemModule

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -1,18 +1,21 @@
 import { CqrsModule } from '@nestjs/cqrs';
+import { FileModule } from 'src/file/file.module';
 import { GetAllItemsHandler } from './application/queries/get-all-items.query';
+import { GetItemThumbnailHandler } from './application/queries/get-item-thumbnail.handler';
 import { ItemController } from './presentation/item.controller';
 import { ItemRepoProvider } from './persistence/item-repository.provider';
 import { itemSchema } from './persistence/item-entity';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-const queryHandlers = [GetAllItemsHandler];
+const queryHandlers = [GetAllItemsHandler, GetItemThumbnailHandler];
 
 @Module({
     controllers: [ItemController],
     exports: [],
     imports: [
         CqrsModule,
+        FileModule,
         MongooseModule.forFeature([
             {
                 name: 'Item',
