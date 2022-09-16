30e227a90f5785a74b3fb9f4caa88aee0893bf56
Pierre <pierre.viara@outlook.com>
2022-09-16 20:24:28 +0200

    tech(version): add UT file for controller

---
+++ src/version/presentation/version.controller.spec.ts
@@ -0,0 +1,15 @@
+import { version } from '../../../package.json';
+import { VersionController } from './version.controller';
+
+describe('VersionController', () => {
+    let sut: VersionController;
+
+    beforeEach(() => (sut = new VersionController()));
+
+    describe('getProjectVersion', () => {
+        it('should return an AppVersion object with current project version', () => {
+            const result = sut.getProjectVersion();
+            expect(result.content).toBe(version);
+        });
+    });
+});
