9c9cc27e199700e9377d5f49df59bebecbf33819
Pierre <pierre.viara@outlook.com>
2022-09-21 15:10:32 +0200

    tech(tests): add test helper

---
+++ src/utils/test-helper.ts
@@ -0,0 +1,57 @@
+import { DynamicModule } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
+import { Test, TestingModule } from '@nestjs/testing';
+
+export class RoutedTestModule {
+    constructor(readonly path: string, readonly module: any) {}
+}
+
+class RouterTestModule {
+    constructor(
+        readonly routerModule: DynamicModule,
+        readonly importedModules: any[],
+    ) {}
+}
+
+export class TestHelper {
+    static async buildTestingModule(
+        routedModules: RoutedTestModule[],
+    ): Promise<TestingModule> {
+        if (routedModules.length === 0) {
+            throw new Error(
+                'Given array of routedModules must contain at least one module.',
+            );
+        }
+
+        const { routerModule, importedModules } =
+            this.buildRouterModule(routedModules);
+        const testingModule = Test.createTestingModule({
+            imports: [routerModule, ...importedModules],
+        });
+
+        return await testingModule.compile();
+    }
+
+    private static buildRouterModule(
+        routedModules: RoutedTestModule[],
+    ): RouterTestModule {
+        const importedModules = [];
+        const routerModule = RouterModule.register([
+            {
+                path: 'api',
+                children: routedModules.map((routedModule) => {
+                    importedModules.push(routedModule.module);
+                    return {
+                        path: routedModule.path,
+                        module: routedModule.module,
+                    };
+                }),
+            },
+        ]);
+
+        return {
+            routerModule,
+            importedModules,
+        };
+    }
+}
