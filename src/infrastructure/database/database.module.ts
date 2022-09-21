3ada3c825c06c46ff805279254a6d03d93f37d3b
Pierre <pierre.viara@outlook.com>
2022-09-21 17:53:55 +0200

    tech(app): move mongoose module to database module

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,10 +1,25 @@
 import { ConfigurationModule } from '../configuration/configuration.module';
 import { DatabaseService } from './database.service';
 import { Module } from '@nestjs/common';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 
 @Module({
     exports: [DatabaseService],
-    imports: [ConfigurationModule],
+    imports: [
+        ConfigurationModule,
+        MongooseModule.forRootAsync({
+            imports: [DatabaseModule],
+            useFactory: (dbService: DatabaseService) => {
+                const options: MongooseModuleOptions = {
+                    uri: dbService.dbURI,
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                };
+                return options;
+            },
+            inject: [DatabaseService],
+        }),
+    ],
     providers: [DatabaseService],
 })
 export class DatabaseModule {}
