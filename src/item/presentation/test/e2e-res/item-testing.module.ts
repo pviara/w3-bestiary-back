4e2b294f11740f82cea6b0ce36321d9677eea2a8
Pierre <pierre.viara@outlook.com>
2022-09-23 15:46:21 +0200

    tech(item): add missing e2e tests

--- src/item/presentation/test/e2e-res/item-testing.module.ts
+++ src/item/presentation/test/e2e-res/item-testing.module.ts
@@ -1,6 +1,7 @@
 import { CqrsModule } from '@nestjs/cqrs';
 import { FileModule } from '../../../../file/file.module';
 import { GetAllItemsHandler } from '../../../application/queries/get-all-items.handler';
+import { GetImageFileHandler } from '../../../../file/application/queries/get-image-file.handler';
 import { ItemController } from '../../item.controller';
 import { itemSchema } from '../../../persistence/item-entity';
 import { ItemTestingRepoProvider } from './item-testing-repo.provider';
@@ -8,7 +9,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TestHelper } from '../../../../utils/test-helper';
 
-const queryHandlers = [GetAllItemsHandler];
+const queryHandlers = [GetAllItemsHandler, GetImageFileHandler];
 
 @Module({
     controllers: [ItemController],
--- src/item/presentation/test/item.controller.e2e-spec.ts
+++ src/item/presentation/test/item.controller.e2e-spec.ts
@@ -12,6 +12,7 @@ describe('ItemController', () => {
     let items: Item[];
 
     const existingLang = 'TEST_LANG';
+    const existingCode = 'blizzard';
 
     beforeAll(async () => {
         const module = await TestHelper.buildTestingModule([
@@ -49,6 +50,30 @@ describe('ItemController', () => {
             );
             expect(response.status).toBe(200);
             expect(response.body).toEqual(items);
+        });
+    });
+
+    describe('GET /thumbnail', () => {
+        it('should return an HTTP error status 400 when no code is given in query', async () => {
+            const response = await request(app.getHttpServer()).get(
+                '/api/item/thumbnail',
+            );
+            expect(response.status).toBe(400);
+        });
+
+        it('should return an HTTP error status 404 when no thumbnail was found with given code', async () => {
+            const response = await request(app.getHttpServer()).get(
+                '/api/item/thumbnail?code=not_existing',
+            );
+            expect(response.status).toBe(404);
+        });
+
+        it('should return an HTTP status 200 with the right thumbnail when given the right code', async () => {
+            const response = await request(app.getHttpServer()).get(
+                `/api/item/thumbnail?code=${existingCode}`,
+            );
+            expect(response.status).toBe(200);
+            expect(Buffer.isBuffer(response.body)).toBe(true);
         });
     });
 
