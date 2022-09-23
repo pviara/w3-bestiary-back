7ed433394e2878ccc4386a84afa2e35f294caed2
Pierre <pierre.viara@outlook.com>
2022-09-23 15:20:40 +0200

    tech(item): add fully working e2e test

--- src/item/presentation/test/e2e-res/item-testing-repo.implement.ts
+++ src/item/presentation/test/e2e-res/item-testing-repo.implement.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Item } from '../../../domain/item';
 import { ItemEntity } from '../../../persistence/item-entity';
+import { ItemMapper } from '../../../../utils/mappers/item.mapper';
 import { ItemRepositoryImplement } from '../../../persistence/item-repository.implement';
-import { Model } from 'mongoose';
+import { AnyKeys, Model } from 'mongoose';
 
 @Injectable()
 export class ItemTestingRepositoryImplement extends ItemRepositoryImplement {
@@ -13,7 +15,31 @@ export class ItemTestingRepositoryImplement extends ItemRepositoryImplement {
         super(_model);
     }
 
-    async getTest() {
-        return await this._model.find({});
+    async createTestingValues(lang: string): Promise<Item[]> {
+        const items = [
+            new Item('test_code-aaa', 'name-aaa'),
+            new Item('test_code-bbb', 'name-bbb'),
+            new Item('test_code-ccc', 'name-ccc'),
+            new Item('test_code-ddd', 'name-ddd'),
+            new Item('test_code-eee', 'name-eee'),
+        ];
+        for (const item of items) {
+            await this._model.create<AnyKeys<ItemEntity>>({
+                code: item.code,
+                names: [
+                    {
+                        lang,
+                        name: item.name,
+                    },
+                ],
+            });
+        }
+
+        const created = await this._model.find({});
+        return ItemMapper.toItems(created);
+    }
+
+    async deleteTestingValues(): Promise<void> {
+        await this._model.deleteMany();
     }
 }
