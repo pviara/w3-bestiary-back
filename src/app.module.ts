110c4873131adbcbf6eaf1f04f99e504acbd2c6e
Pierre <pierre.viara@outlook.com>
2022-09-23 15:19:13 +0200

    tech(app): revert and re-move mongoose module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { CategoryModule } from './category/category.module';
 import { ConfigurationModule } from './infrastructure/configuration/configuration.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
+import { DatabaseService } from './infrastructure/database/database.service';
 import { ItemModule } from './item/item.module';
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { MonsterModule } from './monster/monster.module';
 import { RouterModule } from '@nestjs/core';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -13,7 +15,18 @@ import { VersionModule } from './version/version.module';
     imports: [
         CategoryModule,
         ConfigurationModule,
-        DatabaseModule,
+        MongooseModule.forRootAsync({
+            imports: [DatabaseModule],
+            useFactory: (dbService: DatabaseService) => {
+                const options: MongooseModuleOptions = {
+                    uri: dbService.dbURI,
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                };
+                return options;
+            },
+            inject: [DatabaseService],
+        }),
         ItemModule,
         MonsterModule,
         RouterModule.register([
