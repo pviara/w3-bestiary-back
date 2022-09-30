64189114c82389c061d3bb6bfa2fc743bcf78c32
Pierre <pierre.viara@outlook.com>
2022-09-30 09:50:50 +0200

    tech(version): add swagger doc to controller

--- src/version/domain/version.ts
+++ src/version/domain/version.ts
@@ -1,5 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { version } from '../../../package.json';
 
 export class AppVersion {
+    @ApiProperty({
+        type: String
+    })
     readonly content = version;
 }
--- src/version/presentation/version.controller.ts
+++ src/version/presentation/version.controller.ts
@@ -1,10 +1,17 @@
-import { ApiExcludeController } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppVersion } from '../domain/version';
 import { Controller, Get } from '@nestjs/common';
 
-@ApiExcludeController()
+@ApiTags('version')
 @Controller()
 export class VersionController {
+    @ApiOperation({
+        description: 'Get project version.'
+    })
+    @ApiOkResponse({
+        description: 'Retrieved project version.',
+        type: AppVersion,
+    })
     @Get()
     getProjectVersion(): AppVersion {
         return new AppVersion();
