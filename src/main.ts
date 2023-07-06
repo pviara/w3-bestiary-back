191ac0517e1ba361ba5d09ce8b9cf960f3041376
Pierre Viara <pierre.viara@outlook.com>
2023-07-06 14:35:04 +0200

    fix(app): remove SWC compiler

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { createStream } from 'rotating-file-stream';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { InternalServerErrorException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as morgan from 'morgan';
 import helmet from 'helmet';
 
 async function bootstrap() {
@@ -39,15 +38,6 @@ async function bootstrap() {
         interval: '1d',
         path: LOGS_PATH,
     });
-
-    morgan.token('angle-bracket', () => '>');
-    morgan.token('timestamp', () => new Date().toISOString());
-    app.use(
-        morgan(
-            ':angle-bracket :timestamp :method :url :status :res[content-length] - :response-time ms',
-            { stream: logStream },
-        ),
-    );
 
     const swaggerConfig = new DocumentBuilder()
         .setTitle('The Witcher 3 Bestiary')
